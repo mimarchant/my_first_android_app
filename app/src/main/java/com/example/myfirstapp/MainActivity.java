aa7360bb8f13a4c97139124b26a530073053ba91
mimarchant <mmarchant@moveapps.cl>
2023-06-14 22:01:22 -0400

    primer commit individual 2

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    private int calcular(String numeros) {
+        if (numeros.isEmpty()) {
+            return 0;
+        }
+
+        String[] nums = numeros.split(",");
+        int sum  = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += Integer.valueOf(nums[i]);
+        }
+
+        return sum;
     }
 }
